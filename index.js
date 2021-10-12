8c1a90d07d555065363c4c6cfdf8b66279e32297
Jeremiah Faluyi <faluyijeremiah2018@gmail.com>
2021-10-12 06:45:59 +0100

    route params

--- index.js
+++ index.js
@@ -4,17 +4,32 @@ const app = express();
 
 const port = 8000;
 
-app.get('/', function(req, res){
-    console.log('Request made to root path');
-    res.send('Hello! Welcome to my site! Yay!!!')
-})
-app.get('/about', function(req, res){
+
+app.get('/about/:service/:date', function(req, res){
+
+    console.log(req.params);
+
     console.log('Request made to /about');
     res.send('This is an express course');
 })
 app.get('/contact', function(req, res){
     console.log('Request made to /contact');
     res.send('Subscribe to codemania!')
+})
+
+app.get('/', function(req, res){
+    console.log('Request made to root path');
+    res.send('Hello! Welcome to my site! Yay!!!')
+})
+
+// route parameter
+app.get('/:username', function(req, res){
+
+    const route_user = req.params.username;
+    console.log(req.params);
+
+    console.log(`Request made to root path by ${route_user}`);
+    res.send(`Hello ${route_user}! Welcome to my site! Yay!!!`)
 })
 
 app.listen(port, function(){ 
