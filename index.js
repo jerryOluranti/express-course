d8e2aedba212768031f56da0829a55ab069f7f65
Jeremiah Faluyi <faluyijeremiah2018@gmail.com>
2021-10-15 05:20:25 +0100

    delete, put, patch

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const port = 8000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+const port = 8000;
 
 app.get('/about/:service/:date', function(req, res){
 
@@ -12,6 +14,7 @@ app.get('/about/:service/:date', function(req, res){
     console.log('Request made to /about');
     res.send('This is an express course');
 })
+
 app.get('/contact', function(req, res){
     console.log('Request made to /contact');
     res.send('Subscribe to codemania!')
@@ -23,13 +26,45 @@ app.get('/', function(req, res){
 })
 
 // route parameter
-app.get('/:username', function(req, res){
+// app.get('/:username', function(req, res){
+    
+//     const route_user = req.params.username;
+//     console.log(req.params);
 
-    const route_user = req.params.username;
-    console.log(req.params);
+//     console.log(`Request made to root path by ${route_user}`);
+//     res.send(`Hello ${route_user}! Welcome to my site! Yay!!!`)
+// })
 
-    console.log(`Request made to root path by ${route_user}`);
-    res.send(`Hello ${route_user}! Welcome to my site! Yay!!!`)
+// Post Request
+app.post('/signup', function(req, res) {
+    console.log('Post request made!');
+    console.log(req.body.username);
+    res.json({ name: req.body.name })
+    res.end();
+})
+
+// Delete request
+app.delete('/delete-user', function(req, res) {
+    console.log('Delete request made to the server!');
+    console.log(req.body);
+    res.send('Delete successfull!')
+    res.end();
+})
+
+// Put request
+app.put('/put-user', function(req, res) {
+     console.log("Put request made to the server!");
+     console.log(req.body);
+     res.send("Put successfull!");
+     res.end();
+})
+
+// Put request
+app.patch('/patch-user', function(req, res) {
+     console.log("patch request made to the server!");
+     console.log(req.body);
+     res.send("patch successfull!");
+     res.end();
 })
 
 app.listen(port, function(){ 
