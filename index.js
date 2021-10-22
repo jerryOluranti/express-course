ecd0ff1f94e11c9c321a4d3be7469c4e7861e108
Jeremiah Faluyi <faluyijeremiah2018@gmail.com>
2021-10-22 12:00:50 +0100

    Form handling, file upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer =require('multer');
 
 const app = express();
 
@@ -9,11 +10,31 @@ app.use(express.static('./public'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+//Configuration for Multer
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/upload");
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `${file.fieldname}-${Date.now()}.${ext}`);
+  },
+});
+
+const upload = multer({
+  storage: multerStorage,
+});
+
 const port = 8000;
 
 app.get('/about/:service/:date', function(req, res){
     console.log('Request made to /about');
     res.render('about', { service: req.params.service, date: req.params.date });
+})
+
+app.get('/register', function(req, res){
+    console.log('Request made to /register');
+    res.render('register');
 })
 
 app.get('/contact', function(req, res){
@@ -42,6 +63,19 @@ app.post('/signup', function(req, res) {
     console.log(req.body.username);
     res.json({ name: req.body.name })
     res.end();
+})
+
+app.post('/register', upload.single('avatar'), function(req, res) {
+    console.log(req.file);
+
+    // if (req.body.username === "Jerry"){
+    //     res.send("Login Successfull!");
+    // }else{
+    //     res.send('invalid username!')
+    // }
+
+    res.send('Upload successfull!')
+
 })
 
 // Delete request
