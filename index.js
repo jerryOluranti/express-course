473b7f4b8c1d9f40171cee3101587e389d7bb918
Jeremiah Faluyi <faluyijeremiah2018@gmail.com>
2021-11-02 02:12:41 +0100

    get, post, put, patch, delete

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
-    console.log("request made to /")
+    console.log(`${req.method} request made to /`)
 
     if (req.query.privilege === 'admin'){
         res.send(
@@ -38,6 +41,42 @@ app.get('/videos/:video_id/:video_name', (req, res) => {
 app.get('/profile/:username/:email/:password', (req, res) => {
     console.log(`request made to ${req.url}`);
     res.send(`Welcome ${req.params.username}, your email is: ${req.params.email} and your password is: ${req.params.password}`)
+})
+
+app.post('/signin', (req, res) => {
+    console.log(`${req.method} request made to ${req.url}`);
+
+    console.log(req.body.username);
+
+    res.send('Signin successfull!');
+    res.end();
+})
+
+app.put('/insert-record', (req, res) => {
+    console.log(`${req.method} request made to ${req.url}`);
+
+    console.log(req.body)
+
+    res.send("Record Inserted successfully!");
+    res.end();
+})
+
+app.patch('/update-record', (req, res) => {
+    console.log(`${req.method} request made to ${req.url}`);
+
+    console.log(req.body);
+
+    res.send("Record Updated successfully!");
+    res.end();
+})
+
+app.delete('/delete-record', (req, res) => {
+    console.log(`${req.method} request made to ${req.url}`);
+
+    console.log(req.body);
+
+    res.send("Record Deleted successfully!");
+    res.end();
 })
 
 app.listen(8000, function (){
